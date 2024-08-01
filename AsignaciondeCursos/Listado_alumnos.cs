e8fbc47c111daf5593392869254eb02faf2be5c8
JdanielS7 <jsierrac3@miumg.edu.gt>
2024-07-31 23:38:56 -0600

    Implementación de búsqueda de estudiantes(por completar)

--- AsignaciondeCursos/Listado_alumnos.cs
+++ AsignaciondeCursos/Listado_alumnos.cs
@@ -7,90 +7,112 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace AsignaciondeCursos
 {
     public partial class Listado_alumnos : Form
     {
+        private ConexionMySQL ConexionaMySQL;
+
         public Listado_alumnos()
         {
             InitializeComponent();
             Txt_codCarrera.TextChanged += new EventHandler(Txt_codCarrera_TextChanged);
             Txt_codCur.TextChanged += new EventHandler(Txt_codCur_TextChanged);
+            ConexionaMySQL = new ConexionMySQL();
+            this.Load += new EventHandler(Listado_alumnos_Load);
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void Listado_alumnos_Load(object sender, EventArgs e)
         {
-
+            try
+            {
+                List<Estudiante> estudiantes = ObtenerEstudiantes();
+                Dgv_alumno.DataSource = estudiantes;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar los datos de los estudiantes: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private List<Estudiante> ObtenerEstudiantes()
         {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
+            List<Estudiante> estudiantes = new List<Estudiante>();
+            using (MySqlConnection connection = ConexionaMySQL.GetConnection())
+            {
+                string query = "SELECT ID_ESTUDIANTE, NOMBRE, APELLIDO, FECHA_NAC, CARNE, CREDITOS_ACUMULADOS FROM ESTUDIANTE";
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Estudiante estudiante = new Estudiante
+                            {
+                                Id = reader.GetInt32("ID_ESTUDIANTE"),
+                                Nombre = reader.GetString("NOMBRE"),
+                                Apellido = reader.GetString("APELLIDO"),
+                                FechaNac = reader.GetDateTime("FECHA_NAC"),
+                                Carne = reader.GetInt32("CARNE"),
+                                CreditosAcumulados = reader.GetInt32("CREDITOS_ACUMULADOS")
+                            };
+                            estudiantes.Add(estudiante);
+                        }
+                    }
+                }
+            }
+            return estudiantes;
         }
 
         private void Txt_codCarrera_TextChanged(object sender, EventArgs e)
         {
-            try
-            {
-
-                if (!string.IsNullOrEmpty(Txt_codCarrera.Text))
-                {
-                    long.Parse(Txt_codCarrera.Text);
-                }
-            }
-            catch (FormatException ex)
-            {
-
-                MessageBox.Show(ex.Message, "Entrada inválida", MessageBoxButtons.OK, MessageBoxIcon.Error);
-
-
-                Txt_codCarrera.Text = new string(Txt_codCarrera.Text.Where(char.IsDigit).ToArray());
-
-                Txt_codCarrera.SelectionStart = Txt_codCarrera.Text.Length;
-            }
-            catch (Exception ex)
-            {
-
-                MessageBox.Show($"Ocurrió un error inesperado: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            HandleTextChanged(Txt_codCarrera, "codCarrera");
         }
 
         private void Txt_codCur_TextChanged(object sender, EventArgs e)
         {
+            HandleTextChanged(Txt_codCur, "codCur");
+        }
+
+        private void HandleTextChanged(TextBox textBox, string fieldName)
+        {
             try
             {
-
-                if (!string.IsNullOrEmpty(Txt_codCur.Text))
+                if (!string.IsNullOrEmpty(textBox.Text))
                 {
-                    long.Parse(Txt_codCur.Text);
+                    long.Parse(textBox.Text);
                 }
             }
             catch (FormatException ex)
             {
-
                 MessageBox.Show(ex.Message, "Entrada inválida", MessageBoxButtons.OK, MessageBoxIcon.Error);
-
-
-                Txt_codCur.Text = new string(Txt_codCur.Text.Where(char.IsDigit).ToArray());
-
-                Txt_codCur.SelectionStart = Txt_codCur.Text.Length;
+                textBox.Text = new string(textBox.Text.Where(char.IsDigit).ToArray());
+                textBox.SelectionStart = textBox.Text.Length;
             }
             catch (Exception ex)
             {
-
-                MessageBox.Show($"Ocurrió un error inesperado: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"Ocurrió un error inesperado en {fieldName}: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
+        private void label1_Click(object sender, EventArgs e) { }
+
+        private void label2_Click(object sender, EventArgs e) { }
+
+        private void label3_Click(object sender, EventArgs e) { }
+
+        private void label4_Click(object sender, EventArgs e) { }
+    }
+
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNac { get; set; }
+        public int Carne { get; set; }
+        public int CreditosAcumulados { get; set; }
     }
 }
