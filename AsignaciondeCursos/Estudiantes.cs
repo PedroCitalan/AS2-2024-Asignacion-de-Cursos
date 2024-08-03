bb931418c3f1db7ef38cda23c4078c38665130cd
PedroCitalan <pcitalanh@miumg.edu.gt>
2024-08-02 22:46:13 -0600

    Adición de cambios de la rama morales debido a corrupción de archivos, fueron añadidos de forma aparte, actualización de script SQL

---
+++ AsignaciondeCursos/Estudiantes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsignaciondeCursos
+{
+    class Estudiantes
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Fecha_nacimiento { get; set; }
+        public string Carne { get; set; }
+        public string Creditos_acumulados { get; set; }
+        public string Seccion { get; set; }
+        public string Nombre_usuario { get; set; }
+        public string Contraseña { get; set; }
+        public string Correo_electronico { get; set; }
+        public string tipo_usuario { get; set; }
+        public string ID_Carrera { get; set; }
+        public string ID_Cursos { get; set; }
+
+    }
+}
